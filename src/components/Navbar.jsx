fd4a8a5330f4bbf595cadd7c8e492279381b7f81
Minhajul-alam <alam.minhajul@outlook.com>
2023-10-18 04:27:24 +0600

    Added a responsive navbar with Logo, Manu and User Profile Icon

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+    const navLinks = <>
+        <li><NavLink to={"/"}
+            className={({ isActive, isPending }) =>
+                isPending ? "pending" :
+                    isActive ? "text-gray-400 font-semibold"
+                        : "text-gray-400 font-light"
+            }> Home </NavLink></li>
+    </>
+
+    return (
+        <div>
+            {/* Navbar for md and lg screen */}
+            <div className="navbar max-w-6xl w-[90%] mx-auto hidden lg:flex">
+                <div data-aos="fade-down" data-aos-duration="1000" className="navbar-start">
+                    <a href='/'
+                        className="text-xl font-bold text-gray-400">
+                        <span>auto<span className="text-red-500">Hub</span></span></a>
+                </div>
+
+                <div data-aos="fade-down" data-aos-duration="1000" className="navbar-center flex">
+                    <ul className="menu-horizontal gap-5">
+                        {navLinks}
+                    </ul>
+                </div>
+
+                <div data-aos="fade-down" data-aos-duration="1000" className="navbar-end">
+                    <img className="w-8 rounded-full" src="https://i.ibb.co/r69Q4h6/user-icon-gray.png" />
+                </div>
+            </div>
+
+            {/* Navbar for sm screen */}
+            <div className="navbar w-[90%] mx-auto text-white lg:hidden">
+                <div className="flex-1">
+                    <a href='/'
+                        className="text-xl font-bold text-gray-400">
+                        <span>auto<span className="text-red-500">Hub</span></span></a>
+                </div>
+
+                <div className="flex justify-end flex-1 px-2">
+                    <div className="flex items-stretch">
+                        <div className="dropdown text-blue-950 dropdown-end">
+                            <label tabIndex={0} className="\">
+                                <img className="w-8 rounded-full" src="https://i.ibb.co/r69Q4h6/user-icon-gray.png" />
+                            </label>
+                            <ul tabIndex={0} className="dropdown-content z-[1] p-2 pl-5 shadow bg-base-100 w-40 mt-3">
+                                {navLinks}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
