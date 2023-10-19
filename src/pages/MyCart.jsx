57835d3e1418ac339d1cb678bf268082912974ff
Minhajul-alam <alam.minhajul@outlook.com>
2023-10-19 18:54:39 +0600

    Make Delete button functional of myCart, remove item server and UI both

--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -1,8 +1,59 @@
+import { useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const MyCart = () => {
     const myCarts = useLoaderData();
-    console.log(myCarts);
+    const [carts, setCarts] = useState(myCarts);
+
+    const handelDelete = (_id) => {
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+                confirmButton: 'btn btn-success',
+                cancelButton: 'btn btn-danger'
+            },
+            buttonsStyling: false
+        })
+
+        swalWithBootstrapButtons.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, delete it!',
+            cancelButtonText: 'No, cancel!',
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/myCart/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            swalWithBootstrapButtons.fire(
+                                'Deleted!',
+                                'Your one cart has been deleted.',
+                                'success'
+                            )
+                        }
+                        const remainingCart = myCarts.filter(cart => cart._id !== _id);
+                        setCarts(remainingCart);
+                    })
+            } else if (
+                /* Read more about handling dismissals below */
+                result.dismiss === Swal.DismissReason.cancel
+            ) {
+                swalWithBootstrapButtons.fire(
+                    'Cancelled',
+                    'Your imaginary file is safe :)',
+                    'error'
+                )
+            }
+        })
+    }
+
+
     return (
         <div>
             <div className="h-20">
@@ -11,7 +62,7 @@ const MyCart = () => {
 
             <div className="grid grid-cols-1 md:grid-cols-4 gap-2">
                 {
-                    myCarts.map(myCart => <div className="border border-gray-400 rounded-md space-x-5" key={myCart._id}>
+                    carts.map(myCart => <div className="border border-gray-400 rounded-md space-x-5" key={myCart._id}>
                         <img className="h-56 p-2" src={myCart.productImage} alt="" />
                         <h4 className="text-xl font-bold pb-2">Name: {myCart.productName}</h4>
                         <p className="text-base font-bold">Band Name: {myCart.brandName}</p>
@@ -19,7 +70,7 @@ const MyCart = () => {
                         <p>Price: {myCart.productPrice}$ </p>
                         <p className="mb-2">Rating: {myCart.rating}/10 </p>
                         <Link to={`/productDetail/${myCart._id}`}><button className="btn btn-primary border-none hover:border-none bg-red-500 hover:bg-red-700 text-white mb-2 mr-6">Details</button></Link>
-                        <button className="btn btn-primary border-none hover:border-none bg-red-500 hover:bg-red-700 text-white ">Delete</button>
+                        <button onClick={() => handelDelete(myCart._id)} className="btn btn-primary border-none hover:border-none bg-red-500 hover:bg-red-700 text-white ">Delete</button>
                     </div>)
                 }
             </div>
